f954fb6242dd66187bb9a5e99934531c9d2c82e2
Jeremy Luna <jeremylunaalpha@gmail.com>
2025-05-29 20:10:36 -0500

    making prototype and separated examples

---
+++ signal_analysis/app.js
@@ -0,0 +1,11 @@
+// make array for input registration
+// 
+chunk_audio_inputs = [];
+stream_audio_input = [];
+
+// make array of inputs
+plugins = [];
+
+// import dynamic canvas
+
+// import control panel
