6f6c2a6b3d4d828e8344abb1c0beb9e626f76a25
Jeremy Luna <jeremy.allen.luna@gmail.com>
2025-06-01 00:15:29 -0500

    made the app
    canvases are a nightmare rn, there's no actual canvas elements and i need to setup registration and callbacks.

--- signal_analysis/app.js
+++ signal_analysis/app.js
@@ -1,6 +1,7 @@
 import { ControlPanel } from './controlPanel/controlPanel.js';
 import { DynamicCanvas } from './dynamic_canvas/dynamicCanvas.js';
 import { AudioSamplePuller, MusicPlayer } from './audioPlayer/audioPlayer.js';
+import { STFTVisualizer } from './STFTVisualizer/STFTVisualizer.js';
 
 // TODO:
 // make favicon with https://favicon.io/favicon-converter/
@@ -19,6 +20,7 @@ const control_panel = new ControlPanel();
 // import dynamic canvas
 const dynamic_canvas = new DynamicCanvas();
 
+// make file input
 // make panel elements
 const [audioDetails, audioContent] = control_panel.createDetails("Audio File Input");
 // put them on the panel
@@ -27,6 +29,20 @@ control_panel.tabContent.appendChild(audioDetails);
 const audioPlayer = new MusicPlayer(
   audioContent,
   './audioplayer/SampleProcessor.js'
+);
+// register audioplayer as streamable input
+stream_audio_input.push(audioPlayer);
+
+// make STFT plugin
+// make panel elements
+const [STFTDetails, STFTContent] = control_panel.createDetails("STFT");
+// put them on the panel
+control_panel.tabContent.appendChild(STFTDetails);
+// put the file player in the panel
+const stftVisualizer = new STFTVisualizer(
+  audioPlayer,
+  dynamic_canvas.getCanvas(dynamic_canvas.getAvailableCanvases()[0].id).element,
+  STFTDetails
 );
 // register audioplayer as streamable input
 stream_audio_input.push(audioPlayer);
